0a89e73fb42b7295d079d6e09f996d78d541efde
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-19 22:30:10 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,18 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-  {
-    _putchar('+');
+	{
+		_putchar('+');
 		return (1);
-  }
-  else if (n == 0)
-  {
-    _putchar('0');
-		return (0); 
-  }
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
-  {
-    _putchar('-1');
+	{
+		_putchar('-1');
 		return (-1);
-  }
+	}
 }
