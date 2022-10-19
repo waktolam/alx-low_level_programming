fc0efd6ac2c5d3d09fface957fc6b1478214c242
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-19 22:04:53 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _islower - check if a character is lower
+  * @ch - character
+  * Return: 1 if character is lower else 0
+  */
+int _islower(int ch)
+{
+  if (ch => "a" && ch <= "z")
+    return (1);
+  else
+    return (0);
+}
