f657facfaec5590a6c1e19550064ec0660f597b9
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-19 22:50:10 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
   * _abs - check if a character is alphabet or not
+  * @w: a number
   * Return: a
   */
-int _abs(int)
+int _abs(int w)
 {
-  int w;
-
 	return (w * ((w > 0) - (w < 0)));
 }
