146f2144707098b0c8d86c50a56276a4323051c5
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 21:24:40 +0300

    Create 3-print_remaining_days.c

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @m: month in number format
+* @d: day of month
+* @y: year
+* Return: void
+*/
+
+void print_remaining_days(int m, int d, int y)
+{
+	if (y % 4 == 0 || (y % 400 == 0 && y % 100 == 0))
+	{
+		if (m > 2 && d >= 60)
+		{
+			d++;
+		}
+
+		printf("Day of the year: %d\n", d);
+		printf("Remaining days: %d\n", 366 - d);
+	}
+	else
+	{
+		if (m == 2 && d == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", m, d - 31, y);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", d);
+			printf("Remaining days: %d\n", 365 - d);
+		}
+	}
+}
