12adad4cde90e1beb03ff1d524f2889f2b285936
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 16:17:10 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+  int a;
+
+  if ( n <= 0){
+    _putchar("\n");
+  }
+  else
+  {
+    for (a = 1; a <= n; a++)
+    {
+      printf("_");
+    }
+    _putchar('\n');
+}
