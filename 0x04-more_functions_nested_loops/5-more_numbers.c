f83319e81206075507d77954278af845ad47ea26
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 21:38:40 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,22 @@
 
 /**
  * more_numbers - a function that prints 10 times the numbers, from 0 to 14
- * Return: 0-14 x10 followed by new line
+ *
+ * Return: void
  */
+
 void more_numbers(void)
 {
-	int i, j;
 
+int i, j;
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar('1');
-				_putchar(j % 10 + '0');
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
