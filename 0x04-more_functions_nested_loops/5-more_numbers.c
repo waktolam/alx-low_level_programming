8c98cf32a04ef1ad4a8f23d610cfdaaca2200fd7
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 21:35:46 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i = 0, j;
-	for (i = 0; i < 10; i++)
+
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar("%d", j);
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar("\n");
-	}
+			_putchar('\n');
+		}
 }
