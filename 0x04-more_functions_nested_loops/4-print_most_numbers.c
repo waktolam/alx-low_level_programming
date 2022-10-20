cbdb8bb1c8913c496762210dff65987ca30892d8
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 15:15:00 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,12 +3,13 @@
  * print_most_numbers - prints numbers 0 to 9 skip 2 and 4
  * Return: 0 to 9
  */
+
 void print_most_numbers(void)
 {
-  int i;
-
-  for (i = 0; i < 10; i++)
-    if(i != 2 && i != 4)
-      printf("%d", i);
-  printf("\n");
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			printf("%d", i);
+	printf("\n");
 }
