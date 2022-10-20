11f0b6951b5fbf077c82aa623d4e4dd10e80f529
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-20 14:37:01 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers
- * Return: nothing
+ * Return: 0-9 followed by a new line
  */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_putchar(0 + i);
+		_putchar(i + '0');
 	_putchar("\n");
 }
