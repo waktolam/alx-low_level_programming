e055539783a427f5660c50ca2b4b746765454735
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-26 08:27:39 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i = 0;
 
-	while (s[i++])
-		len++;
+	while (s[i]!='\0')
+		i++;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
