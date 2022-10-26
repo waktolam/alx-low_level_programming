479722ec46c40d517faef064083d8a543c309aae
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-26 08:34:16 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i, len = 0;
+	int i = 0;
 
-	while (str[i++])
-		len++;
-
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
+	for (; str[i] != 0; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
 
 	_putchar('\n');
 }
