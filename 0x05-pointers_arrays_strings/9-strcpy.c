2b339c75ea4dc77e9a89c390128d6b4b87e2ed47
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-26 08:51:41 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (*(src + index) != '\0')
 	{
-		dest[index] = src[index];
+		*(dest + index) = *(src + index);
 		index++;
 	}
-
+	
+	*(dest + index) = '\0';
 	return (dest);
 }
