c496cf91f4864003399d876e79ae378f8e0e9441
waktolam <84005093+waktolam@users.noreply.github.com>
2022-10-27 23:23:58 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = strlen(dest);
-	int a = 0;
+	int index = 0, dest_len = 0;
 
-	while (a < n && *src)
-	{
-		dest[index + a] = *src;
-		src++;
-		a++;
-	}
-	dest[index + a] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
