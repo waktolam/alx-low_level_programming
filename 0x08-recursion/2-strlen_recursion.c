8c243265c0ff3414950687ad1c652a9a6323e422
waktolam <84005093+waktolam@users.noreply.github.com>
2022-11-02 22:53:40 +0300

    Add files via upload

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string.
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
